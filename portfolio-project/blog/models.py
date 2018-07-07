ed89199a51bdd19ab85fde65f585fcfd87402efb
abepena <pena.abe@gmail.com>
2018-07-06 21:21:51 -0400

    blog model done, homepage started in jobs app

--- portfolio-project/blog/models.py
+++ portfolio-project/blog/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Blog(models.Model):
+    """
+    Model for a Blog Post
+    Each blog post will have a title,
+    publication date, body, and image field
+    """
+    title = models.CharField(max_length=200, default=f"Post Title")
+    pub_date = models.DateTimeField()
+    body = models.TextField()
+    image = models.ImageField(upload_to='images/')
+
+    def __str__(self):
+        return self.title
